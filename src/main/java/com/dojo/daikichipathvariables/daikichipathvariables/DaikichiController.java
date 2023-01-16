45443bde59d6682abad0e6269483ed99ef771cf8
Marsi <agalliumars@gmail.com>
2023-01-16 20:14:24 +0100

    Initial commit

---
+++ src/main/java/com/dojo/daikichipathvariables/daikichipathvariables/DaikichiController.java
@@ -0,0 +1,24 @@
+package com.dojo.daikichipathvariables.daikichipathvariables;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("daikichi")
+public class DaikichiController {
+
+    @RequestMapping("/travel/{city}")
+    public String travel(@PathVariable("city") String city){
+        return "Congratulations! You will soon travel to " + city;
+    }
+
+    @RequestMapping("/lotto/{number}")
+    public String evenOrOdd(@PathVariable("number") Integer number){
+        if (number % 2 == 0){
+        return "You will take a grand journey in the near future, but be weary of tempting offers";
+    }
+        return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+}
+}
